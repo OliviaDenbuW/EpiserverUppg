046b828932b0359776d30a0d84c9cb7227ec259b
Olivia Considerit <olivia.denbu@gmail.com>
2019-01-24 12:54:01 +0100

    - Added ImageFile.cs - Added Sufra.jpg in CMS

---
+++ EpiserverUppg/Models/Media/ImageFile.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+using EPiServer.Framework.DataAnnotations;
+
+namespace EpiserverUppg.Models.Media
+{
+    [ContentType(DisplayName = "Image file",
+                 GUID = "8a15d1a2-5f82-4071-84f3-1b3aaa5429ab",
+                 Description = "Use this to upload image files.")]
+    [MediaDescriptor(ExtensionString = "png,gif,jpg,jpeg")]
+    public class ImageFile : ImageData
+    {
+        [CultureSpecific]
+        [Editable(true)]
+        public virtual string Description { get; set; }
+    }
+}
