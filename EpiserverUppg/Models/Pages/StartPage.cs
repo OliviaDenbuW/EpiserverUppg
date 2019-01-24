14325136710e01be7f6654e0db20b596c8440d7b
Olivia Considerit <olivia.denbu@gmail.com>
2019-01-24 11:47:00 +0100

    - Added StartPage.cs with two properties: Heading and MainBody

---
+++ EpiserverUppg/Models/Pages/StartPage.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+using EPiServer.SpecializedProperties;
+
+namespace EpiserverUppg.Models.Pages
+{
+    [ContentType(DisplayName = "Start",
+                 GroupName = SiteGroupNames.Specialized,
+                 Order = 10,
+                 GUID = "8eb9cc9f-a536-414f-9799-91ddac3ea378",
+                 Description = "The home page for a website with an area for blocks and partial pages.")]
+    public class StartPage : PageData
+    {
+        [CultureSpecific]
+        [Display(Name = "Heading", Description = "If the Heading is not set, the page falls back to showing the Name.",
+                 GroupName = SystemTabNames.Content,
+                 Order = 10)]
+        public virtual string Heading { get; set; }
+
+        [CultureSpecific]
+        [Display(Name = "Main body",
+                 Description = "The main body will be shown in the main content area of the page, using the XHTML-editor you can insert for example text, images and tables.",
+                 GroupName = SystemTabNames.Content,
+                 Order = 20)]
+        public virtual XhtmlString MainBody { get; set; }
+}
+}
