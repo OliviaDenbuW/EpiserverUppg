90ee1fe167df3fb16d01d20083d71de9a1b8dcfe
Olivia Considerit <olivia.denbu@gmail.com>
2019-01-24 12:37:42 +0100

    - Added AnyFile.cs in the media folder to enable uplodaing any type of file

---
+++ EpiserverUppg/Models/Media/AnyFile.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+
+namespace EpiserverUppg.Models.Media
+{
+    [ContentType(DisplayName = "Any file",
+                 GUID = "64e151b1-451c-42d4-8198-e499f733be2b",
+                 Description = "Use this to upload any type of file.")]
+    public class AnyFile : MediaData
+    {
+        
+    }
+}
